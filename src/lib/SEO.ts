d6bf5bdd18488fdff73ae0b250240b7afdd919f2
isirmt <r119306@g.kobe-kosen.ac.jp>
2024-08-07 17:44:51 +0900

    fix: metadataBase url

--- src/lib/SEO.ts
+++ src/lib/SEO.ts
@@ -25,7 +25,7 @@ export function generateMetadataTemplate(props: Props): Metadata {
   const outputType: OpenGraphType = type ? type : "website";
 
   const metadata: Metadata = {
-    metadataBase: new URL(outputURL as string),
+    metadataBase: new URL(process.env.NEXT_PUBLIC_URL as string),
     authors: { name: author.name, url: author.url },
     title: outputTitle,
     description: outputDescription,
