3706ca98b33ce4aff88d7f00f55480f887632355
isirmt <r119306@g.kobe-kosen.ac.jp>
2024-09-07 20:43:10 +0900

    add: ogp生成画像にカスタムフォント適用

--- src/app/api/ogp-posts/[...slug]/route.tsx
+++ src/app/api/ogp-posts/[...slug]/route.tsx
@@ -1,20 +1,25 @@
 import { NextRequest } from "next/server";
 import { ImageResponse } from "next/og";
 import { getPost } from "@/lib/getPosts";
+import path from "path";
+import { promises as fs } from "fs";
 
 export async function GET(req: NextRequest, context: { params: { slug: string[] } }) {
   const slug = decodeURIComponent(context.params.slug.join('/'));
   const { data } = await getPost(`${process.env.GIT_POSTS_DIR!}/${slug}.md`);
+  const font = await fs.readFile(path.join(process.cwd(), "assets", "NotoSansJP-ExtraBold-Sub.ttf"));
 
   return new ImageResponse(<div style={{
     width: "100%",
     height: "100%",
+    color: "#222",
     backgroundImage: `url(${process.env.NEXT_PUBLIC_URL}/ogp_back.png)`,
     justifyContent: "center",
     display: "flex",
     alignItems: "center",
     flexDirection: "column",
-    fontSize: "50px"
+    fontSize: "50px",
+    fontFamily: "NotoSansJP",
   }}>
     <div style={{
       width: "70%",
@@ -25,7 +30,14 @@ export async function GET(req: NextRequest, context: { params: { slug: string[] 
     {
       width: 1200,
       height: 630,
-      status: 200
+      status: 200,
+      fonts: [
+        {
+          name: "NotoSansJP",
+          data: font,
+          style: "normal",
+        },
+      ],
     }
   )
 }
