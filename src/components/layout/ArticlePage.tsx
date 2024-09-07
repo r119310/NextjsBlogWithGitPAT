d585df9ca3d41acd2e7adfb380f4610811eed609
isirmt <r119306@g.kobe-kosen.ac.jp>
2024-09-08 08:46:42 +0900

    mod: Article's DateCard position

--- src/components/layout/ArticlePage.tsx
+++ src/components/layout/ArticlePage.tsx
@@ -17,7 +17,7 @@ export default async function Article({ data, content, slug }: { data: PostData,
   const issue = slug ? await getCommentList(slug) : undefined;
 
   return <article className="transition-colors bg-white p-8 rounded-3xl w-full md:w-[34rem] lg:w-[44rem] mx-auto xl:m-0 dark:bg-slate-800">
-    <div className="flex items-center mb-2">
+    <div className="flex flex-col justify-start mb-2 md:flex-row md:items-center">
       <DateCard date={data.date} />
       <h1 className="transition-colors my-4 text-3xl dark:text-white">{data.title}</h1>
     </div>
