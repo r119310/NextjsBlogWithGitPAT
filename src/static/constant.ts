3df8aeadc73cea739866dc07bf0407e59eb39068
r119310 <r119310@g.kobe-kosen.ac.jp>
2024-09-16 14:17:56 +0900

    adjusted for original content

--- src/static/constant.ts
+++ src/static/constant.ts
@@ -1,7 +1,7 @@
-export const siteName = "Mac020k ブログ";
-export const siteDescription = "「Mac020k ブログ」は"
+export const siteName = 'Mac020k ブログ';
+export const siteDescription = '「Mac020k ブログ」は';
 export const author = {
-  name: "Mac020k",
-  url: ""
-}
-export const lastModified = "2024-08-07T09:00:00.000Z";
+  name: 'Mac020k',
+  url: '',
+};
+export const lastModified = '2024-08-07T09:00:00.000Z';
