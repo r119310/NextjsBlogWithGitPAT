140439deb003f0e5b7f138f041e2a425f84a1685
isirmt <r119306@g.kobe-kosen.ac.jp>
2024-09-27 05:12:39 +0900

    add: not-found header mov

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,5 @@
 import { InnerLinkBlueButton } from '@/components/InnerLinkButton';
-import { Main, Section, Side, Title } from '@/components/layout/PageLayout';
+import { Main, SectionNoP, Side, Title } from '@/components/layout/PageLayout';
 import TipsCard from '@/components/TipsCard';
 import { generateMetadataTemplate } from '@/lib/SEO';
 import { Metadata } from 'next';
@@ -17,13 +17,27 @@ export default async function PostList() {
       <Side>
         <TipsCard>ヘッダーより他のページへジャンプ可能です。</TipsCard>
       </Side>
-      <Section>
-        <Title>404 Not Found</Title>
-        <TipsCard>お探しのページは名前が変更されたか、移動・削除された可能性があります。</TipsCard>
-        <div className='flex flex-col items-center justify-center'>
-          <InnerLinkBlueButton path='/post' text='投稿一覧へ' />
+      <SectionNoP>
+        <div className='pointer-events-none m-0 aspect-[10_/_3] w-full overflow-hidden bg-[#0e4589] p-0'>
+          <video
+            src={require('/public/header_mov.mp4')}
+            autoPlay
+            muted
+            loop
+            disableRemotePlayback
+            disablePictureInPicture
+            preload='auto'
+            playsInline
+          />
         </div>
-      </Section>
+        <div className='px-8 pb-8 pt-3'>
+          <Title>404 Not Found</Title>
+          <TipsCard>お探しのページは名前が変更されたか、移動・削除された可能性があります。</TipsCard>
+          <div className='flex flex-col items-center justify-center'>
+            <InnerLinkBlueButton path='/post' text='投稿一覧へ' />
+          </div>
+        </div>
+      </SectionNoP>
     </Main>
   );
 }
