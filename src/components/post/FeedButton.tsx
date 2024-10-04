35ceeebb275eed39efb9de4bf9eedbc7210c7721
isirmt <r119306@g.kobe-kosen.ac.jp>
2024-10-04 09:51:49 +0900

    update: feed button css

--- src/components/post/FeedButton.tsx
+++ src/components/post/FeedButton.tsx
@@ -5,9 +5,9 @@ export default function FeedButton({ url }: { url: string }) {
     <Link className='ml-3 inline-block' target='_blank' rel='noopener noreferrer' href={url}>
       <span
         title='購読する'
-        className='group inline-flex rounded-md border border-blue-400 bg-blue-100 p-1 transition-colors dark:border-violet-400 dark:bg-violet-700 dark:hover:bg-violet-600'
+        className='inline-flex rounded-md border border-blue-400 bg-blue-100 p-1 transition-colors hover:bg-blue-200 dark:border-violet-400 dark:bg-violet-700 dark:hover:bg-violet-600'
       >
-        <span className='i-tabler-rss size-5 bg-blue-400 transition-colors group-hover:bg-yellow-500 dark:bg-violet-400 dark:group-hover:bg-violet-400' />
+        <span className='i-tabler-rss size-5 bg-blue-400 transition-colors dark:bg-violet-400' />
       </span>
     </Link>
   );
