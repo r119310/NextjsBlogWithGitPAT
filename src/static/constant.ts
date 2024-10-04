9452202da0217242ef03d977934933cbc6f5816a
isirmt <r119306@g.kobe-kosen.ac.jp>
2024-10-04 12:11:00 +0900

    update: (my) author url

--- src/static/constant.ts
+++ src/static/constant.ts
@@ -3,7 +3,7 @@ export const siteDescription =
   '「ブログもち」ではプログラミング等で気になった技術・情報等について書き残したりしています';
 export const author = {
   name: 'isirmt',
-  url: 'https://me.itomiri.com',
+  url: 'https://isirmt.com',
 };
 export const lastModified = '2024-08-07T09:00:00.000Z';
 export const enableShowGitHubProfile = true;
